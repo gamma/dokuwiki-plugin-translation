3b18cb70df14d3a8438ced154eadc68a6e107158
Gerry Weißbach <gerry.w@gammaproduction.de>
2023-06-15 12:41:57 +0200

    Do not use require any more

--- action.php
+++ action.php
@@ -10,10 +10,7 @@
 // must be run within Dokuwiki
 if(!defined('DOKU_INC')) die();
 
-if(!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN', DOKU_INC . 'lib/plugins/');
-require_once(DOKU_PLUGIN . 'action.php');
-
-class action_plugin_autotranslation extends DokuWiki_Action_Plugin {
+class action_plugin_autotranslation extends \dokuwiki\Extension\ActionPlugin {
 
     /**
      * For the helper plugin
